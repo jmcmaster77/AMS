2b26cbea443dac84dc75354bd6611397070b4541
Jorge Martin <jmcmaster77@gmail.com>
2024-05-26 07:04:24 -0400

    login dev 70%

--- src/main.py
+++ src/main.py
@@ -1,12 +1,47 @@
 from flask import Flask
+import flask_login, logging
 from routes.glogin import glogin
-from config import FLASK_RUN_HOST, FLASK_RUN_PORT, creator
+from config import FLASK_RUN_HOST, FLASK_RUN_PORT, appinfo, creator, sk
 
 # instancia de la app
 app = Flask(__name__)
 
-# registrando Blueprint 
+# llave de seguridad 
+
+app.secret_key = sk
+
+# login seguridad
+
+login_manager = flask_login.LoginManager()
+
+login_manager.init_app(app)
+
+class User(flask_login.UserMixin):
+    pass
+
+users = {'jmcmaster77@gmail.com': {'password': '1234'}}
+
+@login_manager.user_loader
+def user_loader(email):
+    if email not in users:
+        return
+    print("email", email)
+    user = User()
+    user.id = email
+    return user
+
+# registrando Blueprint
 app.register_blueprint(glogin)
 
+# logger 
+logger = logging.getLogger("waitress")
+logger.setLevel(logging.INFO)
+
 if __name__ == '__main__':
-    app.run(host=FLASK_RUN_HOST, port=FLASK_RUN_PORT)
+    from waitress import serve
+    print(appinfo)
+    print(creator)
+    print("Servidor running on port: ", FLASK_RUN_PORT)
+    serve(app, host=FLASK_RUN_HOST, port=FLASK_RUN_PORT)
+    # app.run(host=FLASK_RUN_HOST, port=FLASK_RUN_PORT)
+    
--- src/routes/glogin.py
+++ src/routes/glogin.py
@@ -1,10 +1,55 @@
-from flask import Blueprint, request, render_template, flash
+from flask import Blueprint, request, redirect, url_for, render_template, flash
+from flask_login import login_required, login_user, logout_user, UserMixin, current_user
 
-# instanciando ruta en blueprint 
+
+# instanciando ruta en blueprint
 
 glogin = Blueprint("login", __name__)
 
-@glogin.route('/login')
+users = {'jmcmaster77@gmail.com': {'password': '1234'}}
+
+
+class User(UserMixin):
+    pass
+
+
+@glogin.route("/")
+def index():
+    print("solicitud get a /")
+    return redirect(url_for("login.login"))
+
+
+@glogin.route('/login', methods=['GET', 'POST'])
 def login():
-    print("Login")
-    return ("login")
+    print("Login get")
+    if request.method == 'GET':
+        return '''
+                <form action='login' method='POST'>
+                    <input type='text' name='email' id='email' placeholder='email'/>
+                    <input type='password' name='password' id='password' placeholder='password'/>
+                    <input type='submit' name='submit'/>
+                </form>
+                '''
+    email = request.form['email']
+    print("userfdb", users)
+    if email in users and request.form['password'] == users[email]['password']:
+        user = User()
+        user.id = email
+        print("user", user.id)  # comentar
+        print("pass")  # comentar
+        login_user(user)
+        return redirect(url_for("login.protected"))
+
+    return 'Bad login'
+
+
+@glogin.route('/protected')
+@login_required
+def protected():
+    return 'Logged in as: ' + current_user.id
+
+@glogin.route('/logout')
+def logout():
+    logout_user()
+    return 'Logged out'
+
