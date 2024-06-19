8f0a656435aa8a1f91af4af52c956b033d679970
Jorge Martin <jmcmaster77@gmail.com>
2024-06-19 01:29:37 -0400

    se valida que el admin id 1 no puede ser modificado por otro admin

--- src/routes/gusuarios.py
+++ src/routes/gusuarios.py
@@ -10,15 +10,20 @@ gu = Blueprint("gusuarios", __name__)
 
 @gu.route("/gusuarios")
 def usuarios():
-    resgistros = Usuarios.query.all()
-    # print("consulta \n", resgistros[0])
-    # for registro in resgistros:
-    #     print("id: ", registro.id)
-    #     print("username: ", registro.username)
-    #     print("fullname: ", registro.fullname)
-    #     print("rol: ", registro.rol)
+    if current_user.rol == 0:
 
-    return render_template("gusuarios/usuarios.html", usuarios=resgistros)
+        resgistros = Usuarios.query.all()
+        # print("consulta \n", resgistros[0])
+        # for registro in resgistros:
+        #     print("id: ", registro.id)
+        #     print("username: ", registro.username)
+        #     print("fullname: ", registro.fullname)
+        #     print("rol: ", registro.rol)
+
+        return render_template("gusuarios/usuarios.html", usuarios=resgistros)
+    else:
+        flash({'title': "AMS", 'message': "Un administrador solo puede gestionar usuarios"}, 'error')
+        return redirect(url_for("home.home_page"))
 
 
 @gu.route("/registraru", methods=["GET", "POST"])
@@ -42,8 +47,7 @@ def registro_usuarios():
 
         # validar que el username no este registrado
 
-        userdata = Usuarios.query.filter_by(
-            username=request.form['username']).first()
+        userdata = Usuarios.query.filter_by(username=request.form['username']).first()
 
         if userdata == None:
             # validacion de que el username no este registrado, realizada
@@ -68,3 +72,32 @@ def registro_usuarios():
         else:
             flash({'title': "AMS", 'message': "Usuario ya registrado"}, 'error')
             return redirect(url_for("gusuarios.registro_usuarios"))
+
+
+@gu.route("/modificaru/<id>", methods=["GET", "POST"])
+def modificaru(id):
+    
+    if current_user.rol == 0:
+        # validar que el id 1 solo pueda ser modificado por el mismo
+        if current_user.id == 1 and id == "1":
+            flash({'title': "AMS", 'message': "se puede editar"}, 'info')
+            return redirect(url_for("gusuarios.usuarios"))
+        elif current_user != 1 and id == "1":
+            flash({'title': "AMS", 'message': "este usuario no puede ser modificado por otro"}, 'info')
+            return redirect(url_for("gusuarios.usuarios"))
+        elif id != 1:
+            flash({'title': "AMS", 'message': "puede modificar el otro usuario"}, 'info')
+            return redirect(url_for("gusuarios.usuarios"))
+    else:
+        flash({'title': "AMS", 'message': "Un administrador solo puede gestionar usuarios"}, 'error')
+        return redirect(url_for("home.home_page"))
+
+
+@gu.route("/eliminaru/<id>", methods=["GET", "POST"])
+def eliminaru(id):
+    if current_user.rol == 0:
+        flash({'title': "AMS", 'message': "en construccion papu"}, 'info')
+        return redirect(url_for("gusuarios.usuarios"))
+    else:
+        flash({'title': "AMS", 'message': "Un administrador solo puede gestionar usuarios"}, 'error')
+        return redirect(url_for("home.home_page"))
