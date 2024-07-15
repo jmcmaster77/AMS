4d97957b9dc2421ca7f2c81edd7932dd45e3d735
Jorge Martin <jmcmaster77@gmail.com>
2024-07-15 12:19:01 -0400

    agregando una compra con los detalles falta verificar la integridad de los datos, se logra visualizar y el modal con una tabla hay que generarlo aparte en el html con otro ciclo for

--- src/routes/gcompras.py
+++ src/routes/gcompras.py
@@ -6,6 +6,7 @@ from models.ModelProductosdb import Productos
 from datetime import datetime
 from utils.db import db
 from utils.log import logger
+import json
 
 gcomp = Blueprint("gcompras", __name__)
 
@@ -49,16 +50,46 @@ def registro_compras():
     else:
 
 
-        print("datos recibidos", request.form)
-        print("Proveedor: ", request.form["proveedor"])
-        print("Cantidad de Item: ", request.form["item"])
+        # print("datos recibidos", request.form)
+        # print("Proveedor: ", request.form["proveedor"])
+        # print("Cantidad de Item: ", request.form["item"])
+
+        data = {}
+        data["productos"] = []
         
         for x in range(int(request.form["item"])):
             if x == 0:
-                print("Producto: ",request.form["producto"], "Cantidad: ", request.form["cantidad"], "Costo: ", request.form["costo"])
+                # print("Producto: ",request.form["producto"], "Cantidad: ", request.form["cantidad"], "Costo: ", request.form["costo"])
+                pdata = Productos.query.filter_by(nombre=request.form['producto']).first()
+                data["productos"].append({
+                    "id": pdata.id,
+                    "nombre": request.form["producto"],
+                    "cantidad":int(request.form["cantidad"]),
+                    "costo":float(request.form["costo"])
+                })
+                totalc = float(request.form["costo"]) * int(request.form["cantidad"])
+                # print("data:",data)
+                # print("Data JSON: ", json.dumps(data))
             else:
-                print(f"Producto "+str(x)+": ",request.form[f"producto"+str(x)], "Cantidad: ", request.form[f"cantidad"+str(x)], "Costo: ", request.form[f"costo"+str(x)])
+                # print(f"Producto "+str(x)+": ",request.form[f"producto"+str(x)], "Cantidad: ", request.form[f"cantidad"+str(x)], "Costo: ", request.form[f"costo"+str(x)])
+                pdata = Productos.query.filter_by(nombre=request.form[f"producto"+str(x)]).first()
+                data["productos"].append({
+                    "id": pdata.id,
+                    "nombre": request.form[f"producto"+str(x)],
+                    "cantidad":int(request.form[f"cantidad"+str(x)]),
+                    "costo":float(request.form[f"costo"+str(x)])
+                })
+                totalc = totalc + (float(request.form[f"costo"+str(x)]) * int(request.form[f"cantidad"+str(x)]))
+        provdata = Proveedores.query.filter_by(fullname=request.form['proveedor']).first()
+        print("Proveedor id", str(provdata.id) + " | " + provdata.fullname )
+        print("Data JSON: ", json.dumps(data))
+        print("Total Compra: ", "%.2f" %totalc)
+        fecha = datetime.now()
+        fecha = fecha.strftime("%Y/%m/%d %H:%M:%S")
 
+        compra = Compras(provdata.id, data, request.form["tcompra"], request.form["mpago"], True, totalc, fecha, False,  current_user.id)
+        db.session.add(compra)
+        db.session.commit()
         logger.info("User id " + str(current_user.id) + " | " + current_user.fullname +
                     " | Registro compra id dev")
         flash({'title': "AMS", 'message': "Compra: " +
