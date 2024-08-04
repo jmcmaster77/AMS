b435c76f8b725d041a7157bc403273b49274bf27
Jorge Martin <jmcmaster77@gmail.com>
2024-08-04 02:01:08 -0400

    Dashboard poblado con los datos consultados aplicando filtros de cantidad de clientes, proveedores, productos, ventas diarias, ventas recientes desc limit 10 y productos con baja disponibilidad de = o menor a 10  necesito que nos ayuden en Venezuela

--- src/routes/home.py
+++ src/routes/home.py
@@ -4,6 +4,12 @@ from flask_login import login_required, current_user
 from utils.log import logger
 from utils.db import db
 from models.ModelTasadb import Tasa
+from models.ModelVentasdb import Ventas
+from models.ModelClientesdb import Clientes
+from models.ModelProveedoresdb import Proveedores
+from models.ModelProductosdb import Productos
+from sqlalchemy import extract
+from datetime import datetime
 
 home = Blueprint("home", __name__)
 
@@ -13,13 +19,13 @@ home = Blueprint("home", __name__)
 def home_page():
     # tasa = Tasa.query.get(1)
     # flash("Todo OK", 'success')
-    # example 
+    # example
     # flash("All OK")
     # flash("All OK", 'success')
     # flash("All Normal", 'info')
     # flash("Not So OK", 'error')
     # flash("So So", 'warning')
-    # example custom titles  
+    # example custom titles
     # flash("Message", 'Custom Title')
     # flash({'title': "Custom Title", 'message': "Error Message"}, 'error')
     # print ("current app:TOASTR_CLOSE_BUTTON | ", current_app.config.get('TOASTR_CLOSE_BUTTON'))
@@ -27,12 +33,50 @@ def home_page():
     current_app.config['TOASTR_CLOSE_BUTTON'] = 'false'
     current_app.config['TOASTR_TIMEOUT'] = '1500'
     # print ("current app: TOASTR_CLOSE_BUTTON |", current_app.config.get('TOASTR_CLOSE_BUTTON'))
-    
-    
-    # app.config['TOASTR_CLOSE_BUTTON'] = 'false'
-    # app.config['TOASTR_TIMEOUT'] = '1000'
-    
-    return render_template("home.html", fullname=current_user.fullname)
+
+    # ============== recaudacion de datos para el dashboard ==========
+
+    # Ultimas 10 ventas
+
+    # ventasR = db.session.query(Ventas).order_by(Ventas.fecha.desc()).limit(10)
+
+    # Ultimas 10 ventas incluyendo el Nombre del Cliente
+
+    ventasR = db.session.query(Clientes.fullname, Ventas.id, Ventas.productos, Ventas.totalv, Ventas.mpago, Ventas.bolivares).filter(
+        Ventas.id_c == Clientes.id).order_by(Ventas.fecha.desc()).limit(10)
+
+    # print("Ventas Recientes:", ventasR)
+    # for vr in ventasR:
+    #     print("Venta:", vr.id)
+
+    tclientes = Clientes.query.count()
+    tproveedores = Proveedores.query.count()
+    tproductos = Productos.query.count()
+
+    # res = session.query(Object).filter(
+    #   extract('month', Object.created) >= datetime.today().month,
+    #   extract('year', Object.created) >= datetime.today().year,
+    #   extract('day', Object.created) >= datetime.today().day)
+    #  ).all()
+
+    # ===== consult total de ventas diarias =============
+
+    tventasd = db.session.query(Ventas).filter(
+        extract('year', Ventas.fecha) == datetime.today().year,
+        extract('month', Ventas.fecha) == datetime.today().month,
+        extract('day', Ventas.fecha) == datetime.today().day
+    ).count()
+
+    # ==== Productos con baja disponibilidad =====
+
+    tproductosdb = db.session.query(Productos).filter(Productos.cantidad <= 10).count()
+
+    productosbd = db.session.query(Productos).filter(Productos.cantidad <= 10).order_by(Productos.fecha.desc()).limit(10)
+
+
+    return render_template("home.html", fullname=current_user.fullname, ventasr=ventasR, tclientes=tclientes,
+                           tproveedores=tproveedores, tproductos=tproductos, tventasd = tventasd,
+                            productosbd = productosbd, tproductosdb = tproductosdb)
 
 
 @home.route("/cambiotasa", methods=["GET", "POST"])
@@ -40,21 +84,24 @@ def home_page():
 def cambiotasa():
     if current_user.rol == 0:
         # tasa = Tasa.query.get(1)
-        tasa = db.session.get(Tasa,1)
+        tasa = db.session.get(Tasa, 1)
 
         tasa.valor = float(request.form["ntasa"])
         db.session.commit()
-        logger.info("User id " + str(current_user.id) + " | " + current_user.fullname + " | tasa de cambio realizada " + str(tasa))
+        logger.info("User id " + str(current_user.id) + " | " +
+                    current_user.fullname + " | tasa de cambio realizada " + str(tasa))
         flash({'title': "AMS", 'message': "Cambio de tasa realizado "}, 'success')
         return redirect("/home")
     else:
         flash({'title': "AMS", 'message': "Un administrador solo puede gestionar cambios de la tasa de cambio del BCV"}, 'error')
         return redirect("/home")
 
+
 @home.route("/acercade")
 @login_required
 def acercade():
     return render_template("acerca.html")
+
 
 @home.route("/error")
 @login_required
